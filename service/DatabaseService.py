55c92a2fe1088dfc34331110b74a71a9561fdfad
Faiz Rashid <mfaizras@gmail.com>
2024-04-16 16:04:09 +0700

    Initial Commit

--- service/DatabaseService.py
+++ service/DatabaseService.py
@@ -1,6 +1,5 @@
 import sqlite3
 from sqlite3 import Error
-import mysql.connector
 from dotenv import load_dotenv
 import os
 
@@ -15,19 +14,6 @@ class SqLite:
         except Error as e:
             print(e)
 
-
-    def cursor(self):
-        return self.db.cursor()
-
-class MySql:
-    def __init__(self):
-        load_dotenv()
-        self.db = mysql.connector.connect(
-            host=os.getenv("MYSQL_HOST"),
-            user=os.getenv("MYSQL_USER"),
-            password=os.getenv("MYSQL_PASSWORD"),
-            database=os.getenv("DB_NAME")
-        )
 
     def cursor(self):
         return self.db.cursor()
